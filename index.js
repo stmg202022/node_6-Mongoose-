38ce44c0104bc1fefc9aaeb49ee3c998046fae7a
Samson Tmg <stmg202023@gmail.com>
2023-03-16 23:37:14 +0545

    added mongoose  and async function name main to connect with localdata_base

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
+const mongoose = require("mongoose");
+
 const productRouter = require("./Routes/product");
 // const userRouter = require("./Routes/user");
+
+//DB_CONNECTION IN MONGOOSE
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+  console.log("DATABASE_CONNECTED SUCCESSFULLY");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 // console.log(process.env);
 // console.log(process.env.DB_PASSWORD);
