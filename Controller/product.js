5b8bd4774c207c0f645d8c89e81a330e76b1d1cc
Samson Tmg <stmg202023@gmail.com>
2023-03-19 08:30:50 +0545

    C R U D operations performs in M O N G O O S E

--- Controller/product.js
+++ Controller/product.js
@@ -7,12 +7,14 @@ const Product = model.Product;
 
 //post
 exports.createProduct = (req, res) => {
-  const product = new Product({
-    title: "iphope",
-    price: 99997,
-    rating: 4.5,
-    brand: "Apple",
-  });
+  const product = new Product(req.body);
+
+  // const product = new Product({
+  //   title: "iphope",
+  //   price: 99997,
+  //   rating: 4.5,
+  //   brand: "Apple",
+  // });
 
   // product.save().then(() => {
   //   res.json(product);
@@ -22,9 +24,10 @@ exports.createProduct = (req, res) => {
     .save()
     .then((result) => {
       console.log(result);
-      res.json({ result });
+      res.status(201).json({ result }); // can be seen in the body
     })
     .catch((err) => {
+      console.log(typeof err);
       console.log(err);
       res.status(500).json({
         error: err,
@@ -38,54 +41,114 @@ exports.createProduct = (req, res) => {
 };
 
 //get
-exports.getAllProducts = (req, res) => {
-  res.json(products);
+// exports.getAllProducts = (req, res) => {
+//   res.json(product);
+// };
+
+exports.getAllProducts = async (req, res) => {
+  // const products = await Product.find({ price: { $gt: 900 } });
+  const products = await Product.find();
+
+  res.status(200).json(products);
 };
 
 //get
-exports.getProduct = (req, res) => {
-  console.log(typeof req.params.id); //string form
-  const id = +req.params.id;
-  console.log(typeof id); //number
-  const product = products.find((p) => p.id === id);
-  res.json(product);
-  // res.json(products);
+// exports.getProduct = (req, res) => {
+//   console.log(typeof req.params.id); //string form
+//   const id = +req.params.id;
+//   console.log(typeof id); //number
+//   const product = products.find((p) => p.id === id);
+//   res.json(product);
+//   // res.json(products);
+// };
+
+exports.getProduct = async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const product = await Product.findById(id);
+  // console.log(product);
+  res.status(200).json(product);
 };
 
 // put
-exports.replaceProduct = (req, res) => {
-  console.log(typeof req.params.id); //string form
-  const id = +req.params.id;
-  console.log(typeof id); //number
-  const productIndex = products.findIndex((p) => p.id === id);
-  products.splice(productIndex, 1, { ...req.body, id: id });
-  res.status(201).json();
+// exports.replaceProduct = (req, res) => {
+//   console.log(typeof req.params.id); //string form
+//   const id = +req.params.id;
+//   console.log(typeof id); //number
+//   const productIndex = products.findIndex((p) => p.id === id);
+//   products.splice(productIndex, 1, { ...req.body, id: id });
+//   res.status(201).json();
+
+//   // res.json(products);
+// };
+
+exports.replaceProduct = async (req, res) => {
+  const id = req.params.id;
+
+  // console.log(id);
+
+  const filter = { _id: id };
+  const replace = req.body;
+  options = { new: true };
+
+  try {
+    const product = await Product.findOneAndReplace(filter, replace, options);
+    console.log(product);
+    res.status(201).json(product);
+  } catch (err) {
+    console.log(err);
+  }
 
   // res.json(products);
 };
 
 //patch
-exports.updateProduct = (req, res) => {
-  console.log(typeof req.params.id); //string form
-  const id = +req.params.id;
-  console.log(typeof id); //number
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex]; //old product
-  products.splice(productIndex, 1, { ...product, ...req.body });
-  res.status(201).json();
-  // res.json(products);
+// exports.updateProduct = (req, res) => {
+//   console.log(typeof req.params.id); //string form
+//   const id = +req.params.id;
+//   console.log(typeof id); //number
+//   const productIndex = products.findIndex((p) => p.id === id);
+//   const product = products[productIndex]; //old product
+//   products.splice(productIndex, 1, { ...product, ...req.body });
+//   res.status(201).json();
+//   // res.json(products);
+// };
+
+exports.updateProduct = async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const filter = { _id: id };
+  const update = req.body;
+  const options = { new: true };
+  try {
+    const product = await Product.findOneAndUpdate(filter, update, options);
+    res.status(201).json(product);
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 //delete
-exports.deleteProduct = (req, res) => {
-  console.log(typeof req.params.id); //string form
-  const id = +req.params.id;
-  console.log(typeof id); //number
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1);
-  res.status(201).json(product);
-  // res.json(products);
-};
+// exports.deleteProduct = (req, res) => {
+//   console.log(typeof req.params.id); //string form
+//   const id = +req.params.id;
+//   console.log(typeof id); //number
+//   const productIndex = products.findIndex((p) => p.id === id);
+//   const product = products[productIndex];
+//   products.splice(productIndex, 1);
+//   res.status(201).json(product);
+//   // res.json(products);
+// };
 
+exports.deleteProduct = async (req, res) => {
+  const id = req.params.id;
+  console.log(typeof id);
+  const filter = { _id: id };
+  try {
+    const product = await Product.findOneAndDelete(filter);
+    res.status(201).json(product);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 // above CRUD are imported in Routes/products
